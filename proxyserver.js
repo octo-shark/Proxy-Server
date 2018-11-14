7fce3d7afc938265490722045d2d78dd7b9896bc
midnightmarth <midnightmarth@gmail.com>
2018-11-14 11:21:50 -0600

    Added temp endpoints and imported axios

--- proxyserver.js
+++ proxyserver.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const axios = require('axios')
 const app = express();
 
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json())
 
+
+//postgres db acceess
+app.get('/postgres/users', (req, res) => {
+  res.send('Idk man.')
+
+})
+
+app.post('/postgres/users', (req, res) => {
+  res.send('posting idk man');
+
+})
+//
+
+//Mongodb access
+app.post('/mongo/time_stamps', (req,res) => {
+  res.send('posting idk man');
+})
+
+app.get('/mongo/time_stamps', (req, res) => {
+  res.send('Idk man.')
+})
 
 app.listen(1128, () => {
   console.log('Server listening on port 1128')
